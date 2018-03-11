66a30f5d5860fe21a588c1dfe771889cafe1129b
Animesh Singh <animesh.singh.jay@gmail.com>
2018-03-11 20:42:20 +0530

    triplet

--- project/threedtotwod.cpp
+++ project/threedtotwod.cpp
@@ -2,14 +2,19 @@
 #include <stdio.h>
 #include <armadillo>
 #include <iostream>
+#include <fstream>
+#include <NGraph>
+
+using namespace NGraph;
+using namespace std;
 
 /*
 
 Input File Format:
 
-1 (int,int,int) : Front viewing direction
-2 (int,int,int) : Top viewing direction
-3 (int,int,int) : Cutting Plane
+1 int int int   : Front viewing direction
+2 int int int   : Top viewing direction
+3 int int int   : Cutting Plane
 4 int v 		: Number of vertices
 5
 .
@@ -25,9 +30,24 @@ Input File Format:
 
 */
 
+struct Triplet
+{
+  int  one_, two_, three_;
+};
 
+vector<Triplet> vertices;
+
+Graph toGraph(char f[])
+{
+	fstream afile;
+	afile.open(f,ios::in);
+	while(afile){
+	}
+	afile.close();
+}
 
 int main(int argc, char const *argv[])
 {
-	
+	Graph G;
+	G = toGraph(argv[0]); //argv[0] is a char array "inputfilename.txt"
 }
