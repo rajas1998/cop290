85afd223cef21b08a03987f0bb89af844e046ef5
Rajas Bansal <rajas1998@gmail.com>
2018-03-12 21:30:14 +0530

    roation fixed

--- project/rajaswork.cpp
+++ project/rajaswork.cpp
@@ -65,27 +65,31 @@ void projectx(){
 		projx.push_back(temp);
 	}
 }
-vector<Triplet> rotate_vector(vector<Triplet> vertices){
+vector<Triplet> rotate_vector(Triplet direction, std::vector<Triplet> vertices){
 	vector<Triplet> rotated_vertices;
+	double x = direction.one;
+	double y = direction.two;
+	double z = direction.three;
+	double size = sqrt(x*x + y*y + z*z);
+	x = x/size;
+	y = y/size;
+	z = z/size;
+	double d = sqrt(y*y + z*z);
+	double cos_alpha = z/d;
+	double sin_alpha = y/d;
+	mat r_x_alpha;
+	r_x_alpha<<1<<0<<0<<endr<<0<<cos_alpha<<(-sin_alpha)<<endr<<0<<sin_alpha<<cos_alpha<<endr;
+	double cos_beta = d;
+	double sin_beta = x;
+	mat r_y_beta;
+	r_y_beta<<cos_beta<<0<<(-sin_beta)<<endr<<0<<1<<0<<endr<<(sin_beta)<<0<<cos_beta<<endr;
+	mat rotatation_matrix = r_y_beta * r_x_alpha;
 	for (int i = 0; i < vertices.size(); ++i)
 	{
 		mat coordinates;
-		double x = vertices.at(i).one;
-		double y = vertices.at(i).two;
-		double z = vertices.at(i).three;
-		coordinates<<x<<endr<<y<<endr<<z<<endr;
-		double d = sqrt(y*y + z*z);
-		double cos_alpha = z/d;
-		double sin_alpha = y/d;
-		mat r_x_alpha;
-		r_x_alpha<<1<<0<<0<<endr<<0<<cos_alpha<<(-sin_alpha)<<endr<<0<<sin_alpha<<cos_alpha<<endr;
-		mat rotated_x = r_x_alpha * coordinates;
-		double cos_beta = d;
-		double sin_beta = x;
-		mat r_y_beta;
-		r_y_beta<<cos_beta<<0<<(-sin_beta)<<endr<<0<<1<<0<<endr<<(sin_beta)<<0<<cos_beta<<endr;
-		mat rotated_y_x = r_y_beta * rotated_x;
-		Triplet temp = {rotated_y_x(0,0), rotated_y_x(1,0), rotated_y_x(2,0)};
+		coordinates<<vertices.at(i).one<<endr<<vertices.at(i).two<<endr<<vertices.at(i).three<<endr;
+		mat new_mat = rotatation_matrix * coordinates;
+		Triplet temp = {new_mat(0,0), new_mat(1,0), new_mat(2,0)};
 		rotated_vertices.push_back(temp);
 	}
 	return rotated_vertices;
@@ -95,6 +99,8 @@ int main(int argc, char const *argv[])
 	std::vector<Triplet> vertices;
 	Triplet temp = {1/sqrt(3),1/sqrt(3),1/sqrt(3)};
 	vertices.push_back(temp);
-	vertices.push_back(temp);
-	cout<<rotate_vector(vertices).at(1).three;
+	Triplet temp2 = {1,1,1};
+	rotate_vector(temp2, vertices).at(0);
+	// vertices.push_back(temp2);
+	// cout<<rotate_vector(vertices).at(1).three;
 }
