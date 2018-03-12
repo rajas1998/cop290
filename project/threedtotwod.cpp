68437a0bd5498fbcd42aee36d989c63b180e0eab
Animesh Singh <animesh.singh.jay@gmail.com>
2018-03-12 20:39:52 +0530

    more projection

--- project/threedtotwod.cpp
+++ project/threedtotwod.cpp
@@ -158,9 +158,123 @@ Graph_Imp Projectionxy(Graph_Imp g)
 	return g1;
 }
 
+Graph_Imp Projectionyz(Graph_Imp g)
+{
+	Graph_Imp g1;
+	Graph A;
+	vector<Triplet> vert;
+	map <int, int> m;
+
+	for (int i = 0; i < g.vertices.size(); ++i)
+	{
+		Triplet temp = {0,g.vertices.at(i).two,g.vertices.at(i).three};
+
+		//This step makes the new vertices and maps old vertices to new ones
+
+		vector<Triplet>::iterator iter;
+		iter = find(vert.begin(), vert.end(), temp);
+	    if(iter != vert.end())
+	    {
+	    	m[i]=(iter - vert.begin());
+	        //m.insert(pair <int, int> (i,(iter - vert.begin())) );
+	    }
+	    else
+	    {
+	        m[i]=(vert.size());
+	        vert.push_back(temp);
+			//m.insert(pair <int, int> (i,vert.size()) );
+	    }
+	}
+	
+ //    map <int, int> :: iterator itr;
+ //    cout << "\nThe map m is : \n";
+ //    cout << "\tKEY\tELEMENT\n";
+ //    for (itr = m.begin(); itr != m.end(); ++itr)
+ //    {
+ //        cout  <<  '\t' << itr->first 
+ //              <<  '\t' << itr->second << '\n';
+ //    }
+
+	//Modifying old graph into new
+	int k=0;
+	for (Graph::const_iterator i = g.edges.begin(); i != g.edges.end(); ++i,++k)
+	{
+		Graph::vertex_set S = Graph::out_neighbors(i);
+		for (Graph::vertex_set::const_iterator p = S.begin(); p != S.end(); ++p)
+		{
+			int d=m[*p];
+			int w=m[k];
+			if(d!=w){
+			A.insert_edge(w,d);
+			A.insert_edge(d,w);
+			}
+		}
+	}
+	g1.vertices=vert;
+	g1.edges=A;
+	return g1;
+}
+
+Graph_Imp Projectionzx(Graph_Imp g)
+{
+	Graph_Imp g1;
+	Graph A;
+	vector<Triplet> vert;
+	map <int, int> m;
+
+	for (int i = 0; i < g.vertices.size(); ++i)
+	{
+		Triplet temp = {g.vertices.at(i).one,0,g.vertices.at(i).three};
+
+		//This step makes the new vertices and maps old vertices to new ones
+
+		vector<Triplet>::iterator iter;
+		iter = find(vert.begin(), vert.end(), temp);
+	    if(iter != vert.end())
+	    {
+	    	m[i]=(iter - vert.begin());
+	        //m.insert(pair <int, int> (i,(iter - vert.begin())) );
+	    }
+	    else
+	    {
+	        m[i]=(vert.size());
+	        vert.push_back(temp);
+			//m.insert(pair <int, int> (i,vert.size()) );
+	    }
+	}
+	
+ //    map <int, int> :: iterator itr;
+ //    cout << "\nThe map m is : \n";
+ //    cout << "\tKEY\tELEMENT\n";
+ //    for (itr = m.begin(); itr != m.end(); ++itr)
+ //    {
+ //        cout  <<  '\t' << itr->first 
+ //              <<  '\t' << itr->second << '\n';
+ //    }
+
+	//Modifying old graph into new
+	int k=0;
+	for (Graph::const_iterator i = g.edges.begin(); i != g.edges.end(); ++i,++k)
+	{
+		Graph::vertex_set S = Graph::out_neighbors(i);
+		for (Graph::vertex_set::const_iterator p = S.begin(); p != S.end(); ++p)
+		{
+			int d=m[*p];
+			int w=m[k];
+			if(d!=w){
+			A.insert_edge(w,d);
+			A.insert_edge(d,w);
+			}
+		}
+	}
+	g1.vertices=vert;
+	g1.edges=A;
+	return g1;
+}
+
 int main(int argc, char const *argv[])
 {
-	Graph_Imp G,G1;
+	Graph_Imp G,G1,G2,G3;
 	G = toGraph("input.txt"); //argv[0] is a char array "inputfilename.txt"
 	for (int i = 0; i < G.vertices.size(); ++i)
 	{
@@ -176,6 +290,9 @@ int main(int argc, char const *argv[])
 		cout<<endl;
 	}
 	G1 = Projectionxy(G);
+	G2 = Projectionyz(G);
+	G3 = Projectionzx(G);
+	
 	for (int i = 0; i < G1.vertices.size(); ++i)
 	{
 		cout<<G1.vertices.at(i).one<<","<<G1.vertices.at(i).two<<","<<G1.vertices.at(i).three<<endl;
@@ -189,5 +306,34 @@ int main(int argc, char const *argv[])
 		}
 		cout<<endl;
 	}
+
+	for (int i = 0; i < G2.vertices.size(); ++i)
+	{
+		cout<<G2.vertices.at(i).one<<","<<G2.vertices.at(i).two<<","<<G2.vertices.at(i).three<<endl;
+	}
+	for (Graph::const_iterator i = G2.edges.begin(); i != G2.edges.end(); ++i)
+	{
+		Graph::vertex_set Si = Graph::out_neighbors(i);
+		for (Graph::vertex_set::const_iterator p = Si.begin(); p != Si.end(); ++p)
+		{
+			cout<<*p<<" ";
+		}
+		cout<<endl;
+	}
+
+	for (int i = 0; i < G3.vertices.size(); ++i)
+	{
+		cout<<G3.vertices.at(i).one<<","<<G3.vertices.at(i).two<<","<<G3.vertices.at(i).three<<endl;
+	}
+	for (Graph::const_iterator i = G3.edges.begin(); i != G3.edges.end(); ++i)
+	{
+		Graph::vertex_set Si = Graph::out_neighbors(i);
+		for (Graph::vertex_set::const_iterator p = Si.begin(); p != Si.end(); ++p)
+		{
+			cout<<*p<<" ";
+		}
+		cout<<endl;
+	}
+
 	return 0;
 }
