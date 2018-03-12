9043b4da75da69e0811236bf42e434471db8c3fa
Rajas Bansal <rajas1998@gmail.com>
2018-03-12 20:47:33 +0530

    added to main file

--- project/threedtotwod.cpp
+++ project/threedtotwod.cpp
@@ -272,6 +272,32 @@ Graph_Imp Projectionzx(Graph_Imp g)
 	return g1;
 }
 
+vector<Triplet> rotate_vector(vector<Triplet> vertices){
+	vector<Triplet> rotated_vertices;
+	for (int i = 0; i < vertices.size(); ++i)
+	{
+		mat coordinates;
+		double x = vertices.at(i).one;
+		double y = vertices.at(i).two;
+		double z = vertices.at(i).three;
+		coordinates<<x<<endr<<y<<endr<<z<<endr;
+		double d = sqrt(y*y + z*z);
+		double cos_alpha = z/d;
+		double sin_alpha = y/d;
+		mat r_x_alpha;
+		r_x_alpha<<1<<0<<0<<endr<<0<<cos_alpha<<(-sin_alpha)<<endr<<0<<sin_alpha<<cos_alpha<<endr;
+		mat rotated_x = r_x_alpha * coordinates;
+		double cos_beta = d;
+		double sin_beta = x;
+		mat r_y_beta;
+		r_y_beta<<cos_beta<<0<<(-sin_beta)<<endr<<0<<1<<0<<endr<<(sin_beta)<<0<<cos_beta<<endr;
+		mat rotated_y_x = r_y_beta * rotated_x;
+		Triplet temp = {rotated_y_x(0,0), rotated_y_x(1,0), rotated_y_x(2,0)};
+		rotated_vertices.push_back(temp);
+	}
+	return rotated_vertices;
+}
+
 int main(int argc, char const *argv[])
 {
 	Graph_Imp G,G1,G2,G3;
